62ee9137d7ac65cb5699e17e9440646c4754b81d
Dmitrii Dimandt <dmitrii@dmitriid.com>
2015-02-19 16:21:24 +0100

    Clean up and create indexes

--- src/grade_populate.erl
+++ src/grade_populate.erl
@@ -10,6 +10,7 @@
 start(Dir) ->
   xref:start(?XREF),
   xref:add_release(?XREF, Dir),
+  ets:new(?MODULE, [named_table, ordered_set, public]),
 
   {ok, Apps} = xref:q(?XREF, "A"),
 
@@ -25,82 +26,64 @@ start(Dir) ->
   io:format("APPS ~p~n", [Apps]),
   lists:foreach(fun(A) -> populate_calls(Neo, A) end, Apps),
 
-  xref:stop(?XREF).
+  neo4j:cypher(Neo, <<"MATCH (app)-[:contains]->(mod)-[:implements]->(fun) "
+                      "set app: `application`, mod :`module`, fun :`function`"
+                    >>),
+
+  xref:stop(?XREF),
+  ets:delete(?MODULE).
 
 
 populate_app(Neo, App) ->
   io:format("Creating app ~p~n", [App]),
   Node = neo4j:create_node(Neo, {[ {<<"name">>, atom_to_binary(App, utf8)} ]}),
-%%   neo4j:add_node_labels(Node, <<"application">>),
 
   {ok, Modules} = xref:q(?XREF, "(Mod) '" ++ atom_to_list(App) ++ "'"),
 
   lists:foreach(fun(M) -> populate_module(Neo, Node, M) end, Modules).
 
 
-populate_module(_, _, alarms_utils) ->
-  ok;
 populate_module(Neo, App, Module) ->
   io:format("... module ~p~n", [Module]),
   Node = neo4j:create_node(Neo, {[ {<<"name">>, atom_to_binary(Module, utf8)} ]}),
-%%   io:format("...   !created~n"),
-%%   neo4j:add_node_labels(Node, <<"module">>),
-%%   io:format("...   !labels~n"),
   neo4j:create_relationship(App, Node, <<"contains">>),
-%%   io:format("...   !contains~n"),
 
   {ok, Funs} = xref:q(?XREF, "'" ++ atom_to_list(Module) ++ "' : Mod * F"),
   lists:foreach(fun(F) -> populate_functions(Neo, Node, F) end, Funs).
 
 
-populate_functions(_Neo, alarms_utils, _) ->
-  ok;
-populate_functions(Neo, Module, {_, F, A}) ->
+populate_functions(Neo, Module, {M, F, A}) ->
   FunName = fun_name(F, A),
-%%   io:format("...... ~p/~p~n", [F, A]),
-  Node = neo4j:create_node(Neo, {[ {<<"name">>, FunName} ]}),
-%%   io:format("......   !created~n"),
-%%   neo4j:add_node_labels(Node, <<"function">>),
-%%   io:format("......   !labels~n"),
-  neo4j:create_relationship(Module, Node, <<"implements">>).
-%%   io:format("......   !implements~n").
-
+  ModName = atom_to_binary(M, utf8),
+  Node = neo4j:create_node(Neo, {[ {<<"name">>, FunName}
+                                 , {<<"mfa">>, <<ModName/binary, ":", FunName/binary>>}]}),
+  neo4j:create_relationship(Module, Node, <<"implements">>),
+  ets:insert(?MODULE, {{M, F, A}, Node}).
 
 
 populate_calls(Neo, App) ->
   NameStr = "'" ++ atom_to_list(App) ++ "'",
-%%   io:format("CALLS BEFORE"),
   {ok, Calls} = xref:q(?XREF, "E ||| " ++ NameStr),
-%%   io:format("CALLS ~p~n", [Calls]),
 
   lists:foreach(fun(C) -> populate_call(Neo, C) end, Calls).
 
-populate_call(Neo, {{FromM, FromF, FromA}, {ToM, ToF, ToA}}) ->
+populate_call(_Neo, {{FromM, FromF, FromA}, {ToM, ToF, ToA}}) ->
+  From = ets:lookup(?MODULE, {FromM, FromF, FromA}),
+  To = ets:lookup(?MODULE, {ToM, ToF, ToA}),
 
-  GetNode = <<"MATCH (n) -[:implements]->(f) "
-              "WHERE n.name={module} AND f.name={fun} "
-              "RETURN f">>,
+  case From of
+    [] ->
+      io:format("Cannot find from: ~p~n", [{FromM, FromF, FromA}]);
+    [{_, FromNode}] ->
+      case To of
+        [] ->
+          io:format("Cannot find to: ~p~n", [{ToM, ToF, ToA}]);
+        [{_, ToNode}] ->
+          neo4j:create_relationship(FromNode, ToNode, <<"calls">>)
+      end
+  end.
 
-  PropsFrom = get_call_props(FromM, FromF, FromA),
-  NodeFrom = data(neo4j:cypher(Neo, GetNode, PropsFrom)),
-%%   io:format("NODEFROM ~p~n", [NodeFrom]),
-
-  PropsTo = get_call_props(ToM, ToF, ToA),
-  NodeTo = data(neo4j:cypher(Neo, GetNode, PropsTo)),
-%%   io:format("NODETO ~p~n", [NodeTo]),
-
-  neo4j:create_relationship(NodeFrom, NodeTo, <<"calls">>).
-
-
-get_call_props(M, F, A) ->
-  {[ {<<"module">>,  atom_to_binary(M, utf8)}
-   , {<<"fun">>, fun_name(F, A)}
-   ]}.
 
 fun_name(F, A) ->
   AB = list_to_binary(integer_to_list(A)),
   <<(atom_to_binary(F, utf8))/binary, "/", AB/binary>>.
-
-data({D}) ->
-  [[N]] = proplists:get_value(<<"data">>, D),
-  N.
