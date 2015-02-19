33b3d5c0d73c6af35c0abca85deed03bbcb027c8
Dmitrii Dimandt <dmitrii@dmitriid.com>
2015-02-19 12:22:13 +0100

    Add app modules to the graph

--- src/grade_populate.erl
+++ src/grade_populate.erl
@@ -15,13 +15,23 @@ start(Dir) ->
 
   Neo = neo4j:connect([{base_uri, ?BASE_URI}]),
 
-  lists:foreach(fun(A) -> populate(Neo, A) end, Apps),
+  lists:foreach(fun(A) -> populate_app(Neo, A) end, Apps),
 
-  lists:foreach(fun(A) ->
   xref:stop(?XREF).
 
 
-populate(Neo, App) ->
-  neo4j:create_node(Neo, {[ {<<"name">>, atom_to_binary(App, utf8)}
-                          , {<<"type">>, <<"application">>}]}),
-  
+populate_app(Neo, App) ->
+  Node = neo4j:create_node(Neo, {[ {<<"name">>, atom_to_binary(App, utf8)}
+                                 , {<<"type">>, <<"application">>}]}),
+  neo4j:add_node_labels(Node, atom_to_binary(App, utf8)),
+
+  {ok, Modules} = xref:q(?XREF, "(Mod) '" ++ atom_to_list(App) ++ "'"),
+
+  lists:foreach(fun(M) -> populate_module(Neo, Node, M) end, Modules).
+
+
+populate_module(Neo, App, Module) ->
+  Node = neo4j:create_node(Neo, {[{<<"name">>, atom_to_binary(Module, utf8)}]}),
+  neo4j:add_node_labels(Node, atom_to_binary(Module, utf8)),
+
+  neo4j:create_relationship(App, Node, <<"contains">>).
